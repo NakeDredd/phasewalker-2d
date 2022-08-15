c9f77302004bca99a5b2b4610c1d8f2457a745fe
Ermolaev Ivan <fnaferm@gmail.com>
2022-08-16 00:49:54 +0700

    [Add] movement, jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int speed;
+    [SerializeField] private int jumpForce;
+    [SerializeField] private float characterHeight;
+
+    [SerializeField] private LayerMask groundCheck;
+
+    private Rigidbody2D rb;
+
+    private float moveInput;
+
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, characterHeight, groundCheck.value);
+        if (Input.GetKey(KeyCode.Space) && hit.collider != null)
+        {
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
+        moveInput = Input.GetAxis("Horizontal") * speed;
+        Vector2 moveVector = new Vector2(moveInput * Time.fixedDeltaTime, rb.velocity.y);
+        rb.velocity = moveVector;
+    }
+
+    private void Jump()
+    {
+        Vector2 jumpVector2 = Vector2.up * jumpForce;
+        rb.AddForce(jumpVector2, ForceMode2D.Impulse);
     }
 }
